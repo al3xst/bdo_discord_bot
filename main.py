e856e0e7d094505a3b1fe6a74f84ec6eb7f715d4
al3xst <al3xst@users.noreply.github.com>
2017-03-23 02:35:41 +0100

    Catch unknown commands
    Catch unknown commands, tell the user as a private message, that the command is not known and try to delete the wrong command from the public channel

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from discord.ext import commands
-
+import discord
 import config
 
 
 bot = commands.Bot(command_prefix=config.BOT_COMMAND_PREFIX)
 
+bot.remove_command("help")  # we will write our own help command
+
 # this specifies what extensions to load when the bot starts up
 startup_extensions = ["failstacks"]
 
-bot.remove_command("help")  # we will write our own help command
+
+@bot.event
+async def on_command_error(error, ctx):
+    if (ctx.message.channel.name not in config.CHANNEL_LIST) and not ctx.message.channel.is_private:
+        #  print(ctx.message.channel.name + " not in " + str(config.CHANNEL_LIST))
+        return
+
+    try:
+        if not ctx.message.channel.is_private:
+            await bot.delete_message(ctx.message)
+    except discord.Forbidden:
+        print("I have no rights, to remove messages in this channel: {}".format(ctx.message.channel.name))
+    return await bot.send_message(ctx.message.author,
+                                  "Command {} not found. Use `!fs help` to list all available commands".format(error))
 
 
 @bot.event
@@ -23,5 +38,6 @@ if __name__ == "__main__":
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
+
 
     bot.run(config.TOKEN)
